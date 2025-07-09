5b091c9b1c92a0e8d7910a1777e304f627473f11
sudam-shrestha <sudamshrestha939@gmail.com>
2025-07-09 20:32:58 +0545

    category crud completed

---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,49 @@
+<x-app-layout>
+    <section class="section">
+        <div class="section-body">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header d-flex justify-content-between">
+                            <h4>Categories</h4>
+                            <a href="{{ route('admin.category.create') }}" class="btn btn-primary">add new</a>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-striped" id="table-1">
+                                    <thead>
+                                        <tr>
+                                            <th class="text-center">
+                                                #
+                                            </th>
+                                            <th>Category Title</th>
+                                            <th>Slug</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($categories as $index => $category)
+                                            <tr>
+                                                <td>
+                                                    {{ ++$index }}
+                                                </td>
+                                                <td>
+                                                    {{ $category->title }}
+                                                </td>
+                                                <td>
+                                                    {{ $category->slug }}
+                                                </td>
+                                                <td><a href="{{ route('admin.category.edit', $category->id) }}"
+                                                        class="btn btn-primary btn-sm">Edit</a></td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\CompanyController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -20,6 +21,7 @@ Route::middleware('auth')->group(function () {
 
 
     Route::resource("admin/company",CompanyController::class)->names("admin.company");
+    Route::resource("admin/category",CategoryController::class)->names("admin.category");
 });
 
 require __DIR__.'/auth.php';
